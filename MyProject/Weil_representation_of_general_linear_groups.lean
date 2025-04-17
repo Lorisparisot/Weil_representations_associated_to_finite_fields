720df3d53db535779bae320aa326dfd94c721eee
LorisParisot <lorisparisot@gmail.com>
2025-04-17 10:20:42 +0200

    weil

--- MyProject/Weil_representation_of_general_linear_groups.lean
+++ MyProject/Weil_representation_of_general_linear_groups.lean
@@ -1,3 +1,17 @@
 import MyProject.convention_dual
 import MyProject.Heisenberg
 import Mathlib.RepresentationTheory.FDRep
+import Mathlib.RepresentationTheory.Character
+
+variable (V k : Type*) [inst1 : Field k] [inst2 : AddCommGroup V] [inst3 : Module k V] [inst4 : FiniteDimensional k V]
+x&
+instance k_addgroup : AddCommGroup k := by
+    exact AddCommGroupWithOne.toAddCommGroup
+
+
+
+
+def tau (a : k) (A : FDRep k (Heisenberg k V)) (ζ : FDRep.character k (↥Subgroup.center (Heisenberg V k))) [hζ : Nontrivial ζ] :
+    (FDRep k k).character := by
+
+sorry
